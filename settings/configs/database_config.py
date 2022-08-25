c3f9800661318db7d0ab2054933e8ddf262290a7
Tamerlan <dzhurunovt@gmail.com>
2022-08-26 03:16:26 +0600

    Fix

--- settings/configs/database_config.py
+++ settings/configs/database_config.py
@@ -9,19 +9,10 @@ POSTGRES_DB = os.environ.get("POSTGRES_DB")
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 
-if ENV == 'test':
-    SQLALCHEMY_DATABASE_URL = (
-        f"{DB_ENGINE}://test_{POSTGRES_USER}:"
-        f"7L357Ugyfds231@test_{POSTGRES_HOST}:"
-        f"{POSTGRES_PORT}/"
-        f"test_{POSTGRES_DB}"
-    )
-
-else:
-    SQLALCHEMY_DATABASE_URL = (
-        f"{DB_ENGINE}://{POSTGRES_USER}:"
-        f"{POSTGRES_PASSWORD}@{POSTGRES_HOST}:"
-        f"{POSTGRES_PORT}/"
-        f"{POSTGRES_DB}"
-    )
+SQLALCHEMY_DATABASE_URL = (
+    f"{DB_ENGINE}://{POSTGRES_USER}:"
+    f"{POSTGRES_PASSWORD}@{POSTGRES_HOST}:"
+    f"{POSTGRES_PORT}/"
+    f"{POSTGRES_DB}"
+)
 
